22d5ca8c1723924da7efa38f2a538fe9b134e7a5
Daniil Blagiy <blagiy.daniil@gmail.com>
2017-11-05 17:38:59 +0300

    implemented writeLog function

--- src/logger.c
+++ src/logger.c
@@ -1,5 +1,7 @@
 #include "logger.h"
 
+#define concat(first, second, third) first second third
+
 Logger setUpConsoleLogger(int prefferedLevel) {
     Logger logger;
 
@@ -34,9 +36,34 @@ Logger setUpFullLogger(int prefferedLevel, const char* logFilePath) {
 }
 
 void writeLog(Logger* logger, LogLevel level, const char* message) {
+    time_t currentTime;
+    currentTime = time(NULL);
 
+    const char* stringLevel;
+    if (level == TRACE)
+        stringLevel = "TRACE";
+    else if (level == DEBUG)
+        stringLevel = "DEBUG";
+    else if (level == INFO)
+        stringLevel = "INFO";
+    else if (level == WARNING)
+        stringLevel = "WARNING";
+    else if (level == ERROR)
+        stringLevel = "ERROR";
+    else if (level == FATAL)
+        stringLevel = "FATAL";
+    else
+        stringLevel = " ";
+
+    if (level >= logger->preferredLevel) {
+        if (logger->printToConsole == TRUE)
+            printf("%s   %s   %s", stringLevel, message,ctime(&currentTime));
+
+        //if (logger->logFilePath != "")
+            //writeToFile();
+    }
 }
 
-void analyseConfigKey(Logger* logger, const char* key) {
-    printf("%s", key);
-}
+/*static void writeToFile() {
+
+}*/
--- test/main.c
+++ test/main.c
@@ -1,7 +1,8 @@
 #include "../src/logger.h"
 
 int main() {
-    Logger logger = setUpFullLogger(0, "d://c dev/Logger/test/log.txt");
+    Logger logger = setUpFileLogger(0, "d://c dev/Logger/test/log.txt");
+    writeLog(&logger, INFO, "Initialization");
 
     return 0;
 }
